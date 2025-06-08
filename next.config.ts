210a175215ca1c4aea06850b84f40d81d7f113f1
fintesia-git <vicente@fintesia.com>
2025-06-08 13:03:55 -0300

    Add www subdomain configuration and redirect from apex domain

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'fintesia.com',
+          },
+        ],
+        destination: 'https://www.fintesia.com/:path*',
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
